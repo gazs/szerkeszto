67f97bad06606eda1739c06903e54536227ce761
Gáspár Körtesi <gazs@gawker.com>
2015-08-15 19:48:35 +0200

    reactify moar

--- range.jsx
+++ range.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 
 class Measurement extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {value: 42};
+	}
+
+	handleChange (e) {
+		this.setState({value: event.target.valueAsNumber});
+	}
+
 	render () {
-		return <div><label>{this.props.name}<input type="range" value={this.props.value} /> {this.props.value}</label></div>
+		return <div><label>{this.props.name}<input type="range" value={this.state.value}  onChange={this.handleChange} /> {this.state.value}</label></div>
+	}
+}
+
+class Formula extends React.Component {
+	render () {
+		return <div><label>{this.props.name}<input type="text" value={this.props.value}  onChange={this.handleChange} /></label></div>
 	}
 }
 
@@ -10,10 +25,18 @@ class Measurements extends React.Component {
 	render () {
 		var createItem = function(name) {
 			var value =	this.props.items[name];
-			return <Measurement name={name} value={value} />
+			return <Measurement name={name} value={value} key={name} />
 		}.bind(this);
 		return <ul>{Object.keys(this.props.items).map(createItem)}</ul>;
 	}
 }
 
-module.exports = Measurements
+class Formulas extends React.Component {
+	render () {
+		return <ul>{Object.keys(this.props.items).map(name =>
+																									<Formula name={name} value={this.props.items[name]} key={name} />)}</ul>;
+	}
+}
+
+
+module.exports = {Measurements, Formulas}
