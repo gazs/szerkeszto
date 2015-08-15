2412e69755795b9bf556a3bec73b8395704a856f
Gáspár Körtesi <gazs@gawker.com>
2015-08-15 20:38:21 +0200

    mukodo csuszkak

--- range.jsx
+++ range.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Measurement extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {value: 42};
+		this.state = {value: this.props.value};
 	}
 
 	handleChange (e) {
@@ -11,30 +11,37 @@ class Measurement extends React.Component {
 	}
 
 	render () {
-		return <div><label>{this.props.name}<input type="range" value={this.state.value}  onChange={this.handleChange} /> {this.state.value}</label></div>
+		return <div><label>{this.props.name}<input type="range" value={this.state.value} onChange={this.handleChange.bind(this)} /> {this.state.value}</label></div>
 	}
 }
 
 class Formula extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {value: this.props.value};
+	}
+	handleChange (e) {
+		this.setState({value: event.target.value});
+	}
 	render () {
-		return <div><label>{this.props.name}<input type="text" value={this.props.value}  onChange={this.handleChange} /></label></div>
+		return <div><label>{this.props.name}<input type="text" value={this.state.value}  onChange={this.handleChange.bind(this)} /></label></div>
 	}
 }
 
 class Measurements extends React.Component {
 	render () {
 		var createItem = function(name) {
-			var value =	this.props.items[name];
+			var value =	this.props.meretek[name];
 			return <Measurement name={name} value={value} key={name} />
 		}.bind(this);
-		return <ul>{Object.keys(this.props.items).map(createItem)}</ul>;
+		return <ul>{Object.keys(this.props.meretek).map(createItem)}</ul>;
 	}
 }
 
 class Formulas extends React.Component {
 	render () {
 		return <ul>{Object.keys(this.props.items).map(name =>
-																									<Formula name={name} value={this.props.items[name]} key={name} />)}</ul>;
+								<Formula name={name} value={this.props.items[name]} key={name} />)}</ul>;
 	}
 }
 
