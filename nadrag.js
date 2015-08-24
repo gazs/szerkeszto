d03fcbb9d82962056159c0af8612d694c9a88334
Gáspár Körtesi <gazs@gawker.com>
2015-08-24 22:08:45 +0200

    vannak tanulságok

--- nadrag.js
+++ nadrag.js
@@ -75,6 +75,12 @@ function render (m, sz) {
 				let vec2d = this.line.toRay2D().getPointAtDistance(distance);
 				return new p(vec2d.x, vec2d.y);
 			}
+			get angle() {
+				return Math.atan2(this.line.b.y - this.line.a.y, this.line.b.x - this.line.a.x)+ mathUtils.radians(270) ///// W T F
+			}
+			get length() {
+				return this.line.getLength();
+			}
 			get a() { return this.line.a}
 			get b() { return this.line.b}
 		}
@@ -124,11 +130,11 @@ function render (m, sz) {
 		points['18'] = intersectionOf(l('4-12').scale(5), l('14a-6').scale(5))
 		points['6a'] = intersectionOf(
 			points[6].horizontalLine(),
-			points[8].longlinePerpendicularToLineWith(points[5])
+			points[8].longlinePerpendicularToLineWith(points[3])
 		)
 		points[19] = intersectionOf(
 			points[2].horizontalLine(),
-			points[8].longlinePerpendicularToLineWith(points[5])
+			points[8].longlinePerpendicularToLineWith(points[3])
 		)
 		points[20] = points[19].right((db / 10) * 5 + 3)
 		points[21] = points[11].right(l('11-20').getLength() / 2)
@@ -140,6 +146,56 @@ function render (m, sz) {
 		points[24] = points[21].left(3 / 2)
 		points[25] = points[21].right(3 / 2)
 		points[26] = points[19].down(0.6)
+
+
+		/// NADRÁG HÁTSÓ RÉSZE
+
+		points['10a'] = points[10].down(3);
+		points['30'] = points['10a'].left(1); // hasasodás esetén + a haskülönbözet két tizede
+		points['12a'] = intersectionOf(
+			points[12].horizontalLine(),
+			l('16-30')
+		)
+		points[31] = points[14].atAngle(new line(points[16], points[14]).angle, 2)
+		points[32] = points[15].atAngle(new line(points[16], points[15]).angle, 2)
+		points[33] = points[13].down(1)
+		points[34] = points[17].left(3 + 0.5)
+		points[35] = points[18].right(3 - 0.5)
+		points[36] = points[30].right(csb / 10 * 4 + 1) // ha bővebb nadrágot készítünk akkor +2 vagy + 3 cm
+		points[37] = points[30].left(csb / 10 * 4 + 1)
+		points[38] = points[34].atAngle(new line(points[34], points[37]).angle, new line(points[17], points[9]).length) // nem pont ott, hanem kicsit ívesebben!
+		points[39] = points[37].right(csb / 10 * 3) // alámenet szélessége
+
+		points[40] = points[19].right(csb / 10)
+		points[41] = points[40].right(csb / 10 * 5 + 4)
+		points[42] = points[35].atAngle(new line(points[35], points[36]).angle, new line(points[18], points[20]).length) // TODO ez egy ív hosszát akarja egy másik ívre rámérni!!!
+		points['40a'] = intersectionOf(
+			points['6a'].horizontalLine(),
+			l('39-40')
+		)
+		points[43] = new p(new Line2D(points['40a'].vec, points[40].vec).copy().scale(2).closestPointTo(points[42].vec))
+		console.warn(new line(points[40], points[43]).length, csb / 10 * 0.5, '40-43 == csb / 10 * 0.5 ellenőrzőméret')
+
+		// with line(42-43)
+		var l42_43 = new line(points[42], points[43])
+		var l43_42 = new line(points[43], points[42])
+		points[44] = points[43].atAngle(l42_43.angle, 0.5)
+		points[45] = points[44].atAngle(l42_43.angle, 2.5)
+		points[46] = points[40].left(2.5)
+		points[48] = points['40a'].left(1.5)
+		points[49] = points['40a'].right(csb + 3 /*a varrásra*/ + 0.5 - l('6-6a').getLength())
+		points[50] = points[44].atAngle(l43_42.angle, db / 10 * 5 + 4)
+		points[51] = points[44].atAngle(l43_42.angle, l('44-42').getLength() / 3 + 2)
+		var hatso_formazovarras1_szelessege = l('50-42').getLength() / 10 * 6;
+		points[52] = points[51].atAngle(l42_43.angle, hatso_formazovarras1_szelessege / 2)
+		points[53] = points[51].atAngle(l43_42.angle, hatso_formazovarras1_szelessege / 2)
+		points[54] = points[51].atAngle(l42_43.angle + mathUtils.radians(270), 14)
+
+		points[55] = points[52].atAngle(l43_42.angle, l('52-42').getLength() / 2)
+		var hatso_formazovarras2_szelessege = l('50-42').getLength() / 10 * 4 / 2
+		points[56] = points[55].atAngle(l42_43.angle, hatso_formazovarras2_szelessege)
+		points[57] = points[55].atAngle(l43_42.angle, hatso_formazovarras2_szelessege)
+		points[58] = points[55].atAngle(l42_43.angle + mathUtils.radians(270), 11)
 
 		function M(p_id) {
 			let p = points[p_id];
@@ -182,6 +238,30 @@ function render (m, sz) {
 			L(25),
 			L(20),
 			"Z")
+
+			paths.hata = path(
+				M(36),
+				L(35),
+				L(31),
+				L(33),
+				L(32),
+				L(34),
+				L(37),
+				L(38),
+				L(8),
+				L('40a'),
+				L('43'),
+				L('53'),
+				L('54'),
+				L('52'),
+				L('56'),
+				L('58'),
+				L('57'),
+				L('42'),
+				L('49'),
+				'Z'
+
+			)
 
 	return {points, paths, lines}
 }
