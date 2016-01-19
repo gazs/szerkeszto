50d3b04668ad3768c12ff5eb599d40163b27a7a5
Gáspár Körtesi <gazs@gawker.com>
2016-01-19 19:54:07 +0100

    some cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,18 @@ var Webpack = require('webpack');
 module.exports = {
   entry: {
     app: "./main.js",
-		editor: "./editor.js"
+		editor: "./editor.js",
+		vendors: [
+			'react',
+			'react-dom',
+			'react-router',
+			'react-redux',
+			'./toxi/geom/Vec2D',
+			'./toxi/geom/Line2D',
+			'./toxi/geom/Ray2D',
+			'./toxi/geom/Circle',
+			'./toxi/math/mathUtils',
+]
   },
   output: {
     path: "./build",
@@ -20,5 +31,9 @@ module.exports = {
 			{ test: /\.ohm$/, exclude: /node_modules/, loader: "ohm-loader"}
 		]
 	},
-	plugins: [new Webpack.HotModuleReplacementPlugin()]
+	plugins: [
+		//new Webpack.HotModuleReplacementPlugin(),
+		new Webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+
+	]
 };
